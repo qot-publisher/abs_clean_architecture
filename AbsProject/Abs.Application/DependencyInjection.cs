2ef783e55160508ba8b5cdf73fa361d0087184e1
Igor <igorzhadko.w@gmail.com>
2024-06-19 17:27:25 +0500

    add LoggingBehavior to MediatR

--- AbsProject/Abs.Application/DependencyInjection.cs
+++ AbsProject/Abs.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Abs.Domain.Bookings;
+﻿using Abs.Application.Abstractions.Behaviors;
+using Abs.Domain.Bookings;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Abs.Application
@@ -10,6 +11,8 @@ namespace Abs.Application
             services.AddMediatR(configuration =>
             {
                 configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+
+                configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
             });
 
             services.AddTransient<PricingService>();
