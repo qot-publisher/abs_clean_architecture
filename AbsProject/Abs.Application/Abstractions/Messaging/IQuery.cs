582ac9d5070ea83194bf2f4030c645532e0b33cc
Igor <igorzhadko.w@gmail.com>
2024-06-19 13:57:08 +0500

    add Messaging abstractions

---
+++ AbsProject/Abs.Application/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using Abs.Domain.Abstractions;
+using MediatR;
+
+namespace Abs.Application.Abstractions.Messaging
+{
+    public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+    {
+    }
+}
---
+++ AbsProject/Abs.Application/Abstractions/Messaging/IQueryHandler.cs
@@ -0,0 +1,11 @@
+﻿
+using Abs.Domain.Abstractions;
+using MediatR;
+
+namespace Abs.Application.Abstractions.Messaging
+{
+    public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>> 
+        where TQuery : IQuery<TResponse>
+    {
+    }
+}
