4017c8b56115fe48063997a5ce5a3d84f8bb6211
Igor <igorzhadko.w@gmail.com>
2024-06-19 13:48:04 +0500

    Create application layer and add dependency injection extension

---
+++ AbsProject/Abs.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Abs.Domain.Bookings;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Abs.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            });
+
+            services.AddTransient<PricingService>();
+
+
+            return services;
+        }
+    }
+}
