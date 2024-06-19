06135b7e1e6f9d0e6cb280485d30ef25b31508ab
Igor <igorzhadko.w@gmail.com>
2024-06-19 16:12:10 +0500

    add GetBooking query

---
+++ AbsProject/Abs.Application/Bookings/GetBooking/GetBookingQuery.cs
@@ -0,0 +1,11 @@
+﻿using Abs.Application.Abstractions.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abs.Application.Bookings.GetBooking
+{
+    public sealed record GetBookingQuery(Guid BookingId): IQuery<BookingResponse>;
+}
