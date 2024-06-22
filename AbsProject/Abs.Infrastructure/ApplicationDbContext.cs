c1656b3493c40774f75923b5ae3dfa15fd90df6a
Igor <igorzhadko.w@gmail.com>
2024-06-22 19:25:07 +0500

    add DbContext

---
+++ AbsProject/Abs.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Abs.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abs.Infrastructure
+{
+    internal class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {           
+        }
+    }
+}
---
+++ AbsProject/Abs.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Abs.Application.Abstractions.Email;
+using Abs.Application.Abstractions.Time;
+using Abs.Infrastructure.Email;
+using Abs.Infrastructure.Time;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Abs.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+            services.AddTransient<IEmailService, EmailService>();
+
+            var conntectionString = configuration.GetConnectionString("Database") 
+                ?? throw new ArgumentNullException(nameof(configuration));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseNpgsql(conntectionString).UseSnakeCaseNamingConvention();
+            });
+
+            return services;
+        }
+    }
+}
