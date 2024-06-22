3f74fcbe41f8f792a102890642f861307fdca36f
Igor <igorzhadko.w@gmail.com>
2024-06-22 19:25:20 +0500

    add EmailService mock

---
+++ AbsProject/Abs.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,17 @@
+﻿using Abs.Application.Abstractions.Email;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abs.Infrastructure.Email
+{
+    internal sealed class EmailService : IEmailService
+    {
+        public Task SendAsync(Domain.Users.Email recipient, string subject, string body)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
