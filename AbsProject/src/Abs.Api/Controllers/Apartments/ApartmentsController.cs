d4f37edd358d7503c725d4a28b15f36cc96828e9
Igor <igorzhadko.w@gmail.com>
2024-06-26 14:39:34 +0500

    create presentation layer, add docker compose

---
+++ AbsProject/src/Abs.Api/Controllers/Apartments/ApartmentsController.cs
@@ -0,0 +1,29 @@
+﻿using Abs.Application.Apartments.SearchApartments;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Abs.Api.Controllers.Apartments
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApartmentsController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public ApartmentsController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchApartments(DateOnly startDate, DateOnly endDate, CancellationToken cancellationToken)
+        {
+            var query = new SearchApartmnetsQuery(startDate, endDate);
+
+            var result = await _sender.Send(query, cancellationToken);
+
+            return Ok(result.Value);
+        }
+    }
+}
---
+++ AbsProject/src/Abs.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Abs.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Abs.Api.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        { 
+            using var scope = app.ApplicationServices.CreateScope();
+
+            using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            dbContext.Database.Migrate();
+        }
+    }
+}
